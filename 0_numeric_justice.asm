f4e56a84d6ffb9f07b6fbcb0abd9550d4657adf2
Almir2021 <90610184+Almir2021@users.noreply.github.com>
2023-06-18 03:35:58 +0200

    Add files via upload
    jnz, jmp instructions.

---
+++ 0_numeric_justice.asm
@@ -0,0 +1,38 @@
+format PE console 
+
+entry start
+
+include 'win32a.inc'
+
+;===========================
+
+section '.text' code readable executable
+
+start:
+
+; The program begins here
+
+call read_hex
+mov  ecx,eax
+call read_hex
+sub  eax,ecx
+jnz l1
+
+
+mov eax,1
+call print_eax
+jmp l2
+
+l1:
+mov  eax,0
+call print_eax
+
+l2:
+
+;Exit the process:
+
+push 0 
+
+call [ExitProcess]
+
+include 'training.inc'
