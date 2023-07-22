51c8b243a110a30d6c778e4dc32d81548237c619
Almir2021 <bosnjicalmir@gmail.com>
2023-07-22 23:21:21 +0200

    retyped solution for better understanding

--- 1_to_prime.asm
+++ 1_to_prime.asm
@@ -9,32 +9,77 @@ entry start
  section 'text' code readable executable
 
  start:
+   
+  call read_hex
 
-  call  read_hex
+  ; original user input in ebx, because we are gonna ese eax in division
 
-   mov ebx,eax
-   xor eax,eax
+  mov ebx,eax
+  ;use a counter for next_n start at 0
 
-   loop1:
-     
-    inc eax
+  mov esi,0 
 
-    call print_eax
+  next_n:
 
-    inc eax
+  inc  esi 
+  mov edx,esi 
+  sub edx,ebx
+  jz  exit 
 
-    dec ebx
-    jz exit_1
-     
-     jmp loop1
-
-     
-
-      exit_1:
+  ; counter for do_division 
+  mov ecx,esi 
 
 
+  do_division:
+
+    dec ecx
+    
+
+    ; when ecx < 2, just print and continue
+
+     mov edx ,ecx
+
+     sub edx, 2
+      
+      js  print_esi 
+
+      ; clear edx for division
+
+      mov edx,0
+      mov eax, esi 
+
+      ; edx:eax / ecx , result is  stored in edx:eax (remainder: quotieant)
+
+       div    ecx
 
 
+
+
+
+
+      
+      ; set the flags
+
+      sub edx,0 
+      jnz do_division
+
+
+      ; if no prime is found go next_n
+
+        jmp next_n
+
+
+        print_esi:
+
+        mov eax,esi 
+
+        call print_eax
+
+        jmp next_n
+
+        exit:
+
+        
        push 0 
 
        call [ExitProcess]
