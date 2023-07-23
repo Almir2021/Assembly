fe66996d87843b5e672ed7a12b1371dc29e7ba3f
Almir2021 <bosnjicalmir@gmail.com>
2023-07-23 23:56:55 +0200

    i have no idea how does it work . Yet

---
+++ 0_mx.asm
@@ -0,0 +1,38 @@
+format PE console
+entry start
+
+include 'win32a.inc'
+
+;====================================
+
+section '.text' code readable executable
+
+
+start:
+
+call read_hex
+mov esi,eax
+mov edx,0
+
+looper:
+
+call read_hex
+
+cmp eax,edx
+
+jbe eax_is_smaller
+mov edx,eax
+
+ eax_is_smaller:
+ dec esi 
+ jnz looper
+
+ mov eax,edx
+ call print_eax
+
+ ;Exit the process
+ push 0 
+
+ call [ExitProcess]
+
+ include 'training.inc'
